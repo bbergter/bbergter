1b1075ff8302b914aa24aa418de5d206a4cea15a
Prof. Dr. Beate Bergter <beate.bergter@htw-berlin.de>
2016-08-07 02:10:15 +0200

    Create aliasses.sh

---
+++ aliasses.sh
@@ -0,0 +1,7 @@
+alias os='open http://localhost:4000'
+alias htw='open http://home.htw-berlin.de/~bergter/'
+alias gh='open http://bbergter.github.io'
+# see http://jekyllrb.com/docs/drafts/
+# see http://jekyllrb.com/docs/drafts/
+alias rj='jekyll serve --drafts'
+cat aliasses.sh
